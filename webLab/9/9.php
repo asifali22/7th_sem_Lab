d9a06aa67daa5aae8b7fc08c426daeb3abdc3163
Asif Ali <monsterx234@gmail.com>
2016-11-30 16:03:28 +0530

    added another comment

--- webLab/9/9.php
+++ webLab/9/9.php
@@ -1,6 +1,7 @@
 <?php
 	date_default_timezone_set('Asia/Calcutta');
 	$months = 60*60*24*60 + time();
+	// Just to print time on date, \o\\n is used, you can also use a -
 	setCookie('lastVisit', date("G:i \o\\n m/d/y"), $months);
 	if(isSet($_COOKIE['lastVisit'])){
 		$visit = $_COOKIE['lastVisit'];
