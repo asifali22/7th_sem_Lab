de2c0609e6400aac7d59f5494a7c55e2a95f490e
Asif Ali <asifalimailme@gmail.com>
2016-12-05 21:25:53 +0530

    Correct 3rd simulation program
    Replaced the stock 3rd program

--- networkLab/partA/3/3.tcl
+++ networkLab/partA/3/3.tcl
@@ -1,10 +1,18 @@
 set ns [new Simulator]
+set f [open out.tr w]
+set nf [open out.nam w]
 
-set tf [open 3.tr w]
-$ns trace-all $tf
-
-set nf [open 3.nam w]
+$ns trace-all $f
 $ns namtrace-all $nf
+
+proc finish {} {
+	global ns f nf
+	$ns flush-trace
+	close $f
+	close $nf
+	exec nam out.nam &
+	exit 0
+}
 
 set n0 [$ns node]
 set n1 [$ns node]
@@ -13,55 +21,59 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 
-$ns duplex-link $n0 $n1 0.01mb 10ms DropTail
-$ns duplex-link $n1 $n2 0.01mb 10ms DropTail
-$ns duplex-link $n2 $n3 0.01mb 10ms DropTail
-$ns duplex-link $n3 $n4 0.01mb 10ms DropTail
-$ns duplex-link $n4 $n5 0.01mb 10ms DropTail
-$ns duplex-link $n0 $n5 0.01mb 10ms DropTail
+$n0 label "ping0"
+$n1 label "ping1"
+$n2 label "R1"
+$n3 label "R2"
+$n4 label "ping4"
+$n5 label "ping5"
 
-Agent/Ping instproc recv { from $rtt } {
+$ns color 1 red
+$ns color 2 blue
+$ns color 3 green
+$ns color 4 orange
+
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 0.4Mb 30ms DropTail
+$ns duplex-link $n3 $n4 1Mb 10ms DropTail
+$ns duplex-link $n3 $n5 1Mb 10ms DropTail
+
+set ping0 [new Agent/Ping]
+$ns attach-agent $n0 $ping0
+
+set ping1 [new Agent/Ping]
+$ns attach-agent $n1 $ping1
+
+set ping4 [new Agent/Ping]
+$ns attach-agent $n4 $ping4
+
+set ping5 [new Agent/Ping]
+$ns attach-agent $n5 $ping5
+
+$ns connect $ping0 $ping4
+$ns connect $ping1 $ping5
+
+proc sendPingPacket {} {
+	global ns ping0 ping1
+	set intervalTime 0.001
+	set now [$ns now]
+	$ns at [expr $now + $intervalTime] "$ping0 send"
+	$ns at [expr $now + $intervalTime] "$ping1 send"
+	$ns at [expr $now + $intervalTime] "sendPingPacket"
+}
+
+Agent/Ping instproc recv {from rtt} {
+	global seq
 	$self instvar node_
-	puts "node [$node_id] received ping answer  $from with rount-trip-time of $rtt ms"
+	puts "The node [$node_ id] received an ACK from the node $from with RTT $rtt ms"
 }
 
-set P(0) [new Agent/Ping]
-$ns attach-agent $n0 $P(0)
-set P(1) [new Agent/Ping]
-$ns attach-agent $n1 $P(1)
-set P(2) [new Agent/Ping]
-$ns attach-agent $n2 $P(2)
-set P(3) [new Agent/Ping]
-$ns attach-agent $n3 $P(3)
-set P(4) [new Agent/Ping]
-$ns attach-agent $n4 $P(4)
-set P(5) [new Agent/Ping]
-$ns attach-agent $n5 $P(5)
+$ping0 set class_ 1
+$ping1 set class_ 2
+$ping4 set class_ 4
+$ping5 set class_ 5
 
-$ns connect $P(0) $P(1)
-$ns connect $P(1) $P(2)
-$ns connect $P(2) $P(3)
-$ns connect $P(3) $P(4)
-$ns connect $P(4) $P(5)
-$ns connect $P(5) $P(0)
-
-for { set i 0 } { $i<50 } { incr i } {
-	$ns at 0.2 "$P(0) send"
-	$ns at 0.2 "$P(1) send"
-	$ns at 0.2 "$P(2) send"
-	$ns at 0.2 "$P(3) send"
-	$ns at 0.2 "$P(4) send"
-	$ns at 0.2 "$P(5) send"
-}
-
-proc finish { } {
-	global ns tf nf
-	$ns flush-trace
-	exec nam 3.nam &
-	close $tf
-	close $nf
-	exit 0
-}
-
-$ns at 2.0 "finish"
-$ns run
+$ns at 0.01 "sendPingPacket"
+$ns at 10.0 "finish"
+$ns run 
